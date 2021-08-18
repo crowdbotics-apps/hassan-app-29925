68cf590439581fedec2eae79d60c2b189c84bf82
Crowdbotics <team@crowdbotics.com>
2021-08-18 19:59:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "hassan_app_29925Identifier",
+  url: "https://hassan_app_29925Identifier.botics.co",
+  api: "https://hassan_app_29925Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
